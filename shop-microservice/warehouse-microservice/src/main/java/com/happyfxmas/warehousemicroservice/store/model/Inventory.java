689257f5cb3317be91c2a38aef29e1383a6a3152
HappyMq <happy.xmas1@mail.ru>
2024-01-15 23:51:04 +0300

    changed to one-to-one

--- shop-microservice/warehouse-microservice/src/main/java/com/happyfxmas/warehousemicroservice/store/model/Inventory.java
+++ shop-microservice/warehouse-microservice/src/main/java/com/happyfxmas/warehousemicroservice/store/model/Inventory.java
@@ -1,5 +1,6 @@
 package com.happyfxmas.warehousemicroservice.store.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
@@ -8,7 +9,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -40,7 +41,11 @@ public class Inventory {
     @LastModifiedDate
     private Instant updatedAt;
 
-    @ManyToOne(fetch = FetchType.EAGER, optional = false)
-    @JoinColumn(name = "product_id")
+    @OneToOne(
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.MERGE,
+            optional = false
+    )
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
     private Product product;
 }
