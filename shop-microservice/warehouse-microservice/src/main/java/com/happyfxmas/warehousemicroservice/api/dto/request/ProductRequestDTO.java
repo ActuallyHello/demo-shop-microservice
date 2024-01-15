2771a768438e6822d623364fa5461429744e3812
HappyMq <happy.xmas1@mail.ru>
2024-01-15 19:55:24 +0300

    refactor

--- shop-microservice/warehouse-microservice/src/main/java/com/happyfxmas/warehousemicroservice/api/dto/request/ProductRequestDTO.java
+++ shop-microservice/warehouse-microservice/src/main/java/com/happyfxmas/warehousemicroservice/api/dto/request/ProductRequestDTO.java
@@ -1,12 +1,9 @@
 package com.happyfxmas.warehousemicroservice.api.dto.request;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
-import java.util.UUID;
 
 @AllArgsConstructor
 @Getter
