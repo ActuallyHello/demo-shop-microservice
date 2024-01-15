cb8d7e3d2109b7a8503b2f7baa21d0cb2bb19c95
HappyMq <happy.xmas1@mail.ru>
2024-01-15 23:53:41 +0300

    refactor and get by id with inventory

--- shop-microservice/warehouse-microservice/src/main/java/com/happyfxmas/warehousemicroservice/api/controller/ProductController.java
+++ shop-microservice/warehouse-microservice/src/main/java/com/happyfxmas/warehousemicroservice/api/controller/ProductController.java
@@ -1,10 +1,12 @@
 package com.happyfxmas.warehousemicroservice.api.controller;
 
 import com.happyfxmas.warehousemicroservice.api.dto.ProductDTO;
+import com.happyfxmas.warehousemicroservice.api.dto.response.ProductWithInventoryDTO;
 import com.happyfxmas.warehousemicroservice.api.dto.request.ProductRequestDTO;
 import com.happyfxmas.warehousemicroservice.api.dto.request.ProductUpdateRequestDTO;
 import com.happyfxmas.warehousemicroservice.api.dto.response.ProductWithSupplierDTO;
 import com.happyfxmas.warehousemicroservice.api.mapper.ProductMapper;
+import com.happyfxmas.warehousemicroservice.api.mapper.ProductWithInventoryMapper;
 import com.happyfxmas.warehousemicroservice.api.mapper.ProductWithSupplierMapper;
 import com.happyfxmas.warehousemicroservice.exception.response.ProductNotFoundException;
 import com.happyfxmas.warehousemicroservice.exception.response.ProductServerException;
@@ -13,7 +15,6 @@ import com.happyfxmas.warehousemicroservice.exception.service.ProductCreationExc
 import com.happyfxmas.warehousemicroservice.exception.service.ProductDeleteException;
 import com.happyfxmas.warehousemicroservice.service.ProductService;
 import com.happyfxmas.warehousemicroservice.service.SupplierService;
-import com.happyfxmas.warehousemicroservice.store.model.enums.ProductType;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -44,7 +45,8 @@ public class ProductController {
     private final SupplierService supplierService;
 
     private static final String BY_ID = "/{id}";
-    private static final String WITH_SUPPLIERS = "/suppliers";
+    private static final String WITH_SUPPLIER = "/suppliers";
+    private static final String WITH_INVENTORY = "/inventories";
 
     @GetMapping
     public ResponseEntity<List<ProductDTO>> getAllProducts(@RequestParam(required = false) Integer page,
@@ -68,11 +70,20 @@ public class ProductController {
         return ResponseEntity.ok(ProductMapper.makeDTO(product));
     }
 
-    @GetMapping(BY_ID + WITH_SUPPLIERS)
+    @GetMapping(BY_ID + WITH_SUPPLIER)
     public ResponseEntity<ProductWithSupplierDTO> getProductWithSupplierById(@PathVariable UUID id) {
         var product = productService.getByIdWithSupplier(id)
-                .orElseThrow(() -> new ProductNotFoundException("Product with id=%s does not exist!".formatted(id)));
+                .orElseThrow(() -> new ProductNotFoundException(
+                        "ProductSupplier with id=%s does not exist!".formatted(id)));
         return ResponseEntity.ok(ProductWithSupplierMapper.makeDTO(product, product.getSupplier()));
+    }
+
+    @GetMapping(BY_ID + WITH_INVENTORY)
+    public ResponseEntity<ProductWithInventoryDTO> getProductWithInventoryById(@PathVariable UUID id) {
+        var product = productService.getByIdWithInventory(id)
+                .orElseThrow(() -> new ProductNotFoundException(
+                        "ProductInventory with id=%s does not exist!".formatted(id)));
+        return ResponseEntity.ok(ProductWithInventoryMapper.makeDTO(product, product.getInventory()));
     }
 
     @PostMapping
@@ -92,26 +103,11 @@ public class ProductController {
 
     @PatchMapping(BY_ID)
     public ResponseEntity<ProductDTO> updateProductById(@PathVariable UUID id,
-                                                  @RequestBody @Valid ProductUpdateRequestDTO productUpdateRequestDTO) {
+                                                        @RequestBody @Valid ProductUpdateRequestDTO productUpdateRequestDTO) {
         var product = productService.getByIdWithSupplier(id)
                 .orElseThrow(() -> new ProductNotFoundException("Product with id=%s does not exist!".formatted(id)));
-        if (productUpdateRequestDTO.getSupplierId() != null) {
-            var supplier = supplierService.getById(UUID.fromString(productUpdateRequestDTO.getSupplierId()))
-                    .orElseThrow(() -> new SupplierNotFoundException(
-                            "Supplier with id=%s was not found!".formatted(productUpdateRequestDTO.getSupplierId())));
-            product.setSupplier(supplier);
-        }
-        if (productUpdateRequestDTO.getTitle() != null) {
-            product.setTitle(productUpdateRequestDTO.getTitle());
-        }
-        if (productUpdateRequestDTO.getDescription() != null) {
-            product.setDescription(productUpdateRequestDTO.getDescription());
-        }
-        if (productUpdateRequestDTO.getType() != null) {
-            product.setType(ProductType.valueOf(productUpdateRequestDTO.getType()));
-        }
         try {
-            product = productService.update(product);
+            product = productService.update(productUpdateRequestDTO, product);
             return ResponseEntity.ok(ProductMapper.makeDTO(product));
         } catch (ProductCreationException exception) {
             throw new ProductServerException(exception.getMessage(), exception);
@@ -120,7 +116,7 @@ public class ProductController {
 
     @DeleteMapping(BY_ID)
     public ResponseEntity<?> deleteProductById(@PathVariable UUID id) {
-        var product = productService.getByIdWithSupplier(id)
+        var product = productService.getById(id)
                 .orElseThrow(() -> new ProductNotFoundException("Product with id=%s does not exist!".formatted(id)));
         try {
             productService.delete(product);
