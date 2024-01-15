10fa33df8aae7cbb6ec7a471c0b3f2f8579b2594
HappyMq <happy.xmas1@mail.ru>
2024-01-15 23:51:23 +0300

    findByProduct

--- shop-microservice/warehouse-microservice/src/main/java/com/happyfxmas/warehousemicroservice/store/repository/InventoryRepository.java
+++ shop-microservice/warehouse-microservice/src/main/java/com/happyfxmas/warehousemicroservice/store/repository/InventoryRepository.java
@@ -1,9 +1,13 @@
 package com.happyfxmas.warehousemicroservice.store.repository;
 
 import com.happyfxmas.warehousemicroservice.store.model.Inventory;
+import com.happyfxmas.warehousemicroservice.store.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface InventoryRepository extends JpaRepository<Inventory, UUID> {
+
+    Optional<Inventory> findByProduct(Product product);
 }
